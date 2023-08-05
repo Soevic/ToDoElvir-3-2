cf63559b07ad084b8c62733976be59e39940b040
Sergei <serg_shatilov@icloud.com>
2023-08-05 13:37:21 +0300

    Initial Commit

---
+++ ToDoElvir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoElvir
+//
+//  Created by Sergey Shatilov on 05.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
